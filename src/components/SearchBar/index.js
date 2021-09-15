f15e12989bb52b65e837fc89e8c3b93480fc44c9
Jason Wilson <jasonwilson@MacBook-Pro.home>
2021-09-15 02:43:15 -0500

    updated-LKG

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,12 +1,12 @@
-import styles from './Seach.module.css'
 
-const SearchBar = ({...rest}) => {
-    return (
-        <div className={styles.coin_search}>
-            {/* <h1>Search</h1> */}
-            <input className={styles.coin_input} {...rest}/>
-        </div>
-    )
-}
+import styles from './Search.module.css';
+
+const SearchBar = ({ ...rest }) => {
+  return (
+    <div className={styles.coin_search}>
+      <input className={styles.coin_input} {...rest} />
+    </div>
+  );
+};
 
 export default SearchBar;
